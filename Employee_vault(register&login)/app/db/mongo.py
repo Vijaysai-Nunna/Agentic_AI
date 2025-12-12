bd96e069c0fc0427771661ea7c6e49faafc7c60f
unknown <Your Name>
2025-12-12 16:25:19 +0530

    Removed

--- Employee_vault(register&login)/app/db/mongo.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from app.config import MONGO_URL, DB_NAME
-
-client = MongoClient(MONGO_URL)
-db = client[DB_NAME]
-collection = db['sampled_data']
-
--- POC_projects/Expense_tracker_FASTAPI(POC)/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers import expenses
-from logger import logger
-
-
-
-app = FastAPI()
-app.include_router(expenses.router)
-logger.info("Expense Tracker API started")  
