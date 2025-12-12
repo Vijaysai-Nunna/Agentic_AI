35dd0198061481c1af1ab3cae473cf8f8ef9235c
unknown <Your Name>
2025-12-12 17:06:08 +0530

    Organized

--- Agentic_AI/Python/day-1_list.py
+++
@@ -1,9 +0,0 @@
-fruitlist=["apple","banana","grape"]
-fruitlist.append("mango")
-fruitlist.append("pineapple")
-
-fruitlist.insert(1,"apple")
-fruitlist.insert(-2,"xyz")
-
-fruitlist.remove("apple")
-print(fruitlist)
